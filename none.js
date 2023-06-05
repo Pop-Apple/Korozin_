f2c0ad855d96433192f2509f0e851f9e67c6cfae
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-06-05 11:26:11 +0900

    NO ❌

---
+++ none.js
@@ -0,0 +1,156 @@
+/*
+function Typing() {
+
+    setTimeout(function() {
+
+        document.getElementById('a').style.display = "contents"
+
+        setTimeout(function() {
+
+            document.getElementById('b').style.display = "contents"
+
+            setTimeout(function() {
+
+                document.getElementById('c').style.display = "contents"
+
+                setTimeout(function() {
+
+                    document.getElementById('e').style.display = "contents"
+
+                    setTimeout(function() {
+
+                        document.getElementById('f').style.display = "contents"
+
+                        setTimeout(function() {
+
+                            document.getElementById('g').style.display = "contents"
+
+                            setTimeout(function() {
+
+                                document.getElementById('h').style.display = "contents"
+
+                                setTimeout(function() {
+
+                                    document.getElementById('i').style.display = "contents"
+
+                                    setTimeout(function() {
+
+                                        document.getElementById('j').style.display = "contents"
+
+                                        setTimeout(function() {
+
+                                            document.getElementById('k').style.display = "contents"
+
+                                            setTimeout(function() {
+
+                                                document.getElementById('l').style.display = "contents"
+
+                                                setTimeout(function() {
+
+                                                    document.getElementById('m').style.display = "contents"
+
+                                                    setTimeout(function() {
+
+                                                        document.getElementById('n').style.display = "contents"
+
+                                                        setTimeout(function() {
+
+                                                            document.getElementById('o').style.display = "contents"
+
+                                                            setTimeout(function() {
+
+                                                                document.getElementById('p').style.display = "contents"
+
+                                                                setTimeout(function() {
+
+                                                                    document.getElementById('q').style.display = "contents"
+
+                                                                    setTimeout(function() {
+
+                                                                        document.getElementById('r').style.display = "contents"
+
+                                                                        setTimeout(function() {
+
+                                                                            document.getElementById('s').style.display = "contents"
+
+                                                                            setTimeout(function() {
+
+                                                                                document.getElementById('t').style.display = "contents"
+
+                                                                                setTimeout(function() {
+
+                                                                                    document.getElementById('u').style.display = "contents"
+
+                                                                                    setTimeout(function() {
+
+                                                                                        document.getElementById('a').style.display = "none"
+                                                                                        document.getElementById('b').style.display = "none"
+                                                                                        document.getElementById('c').style.display = "none"
+                                                                                        document.getElementById('e').style.display = "none"
+                                                                                        document.getElementById('f').style.display = "none"
+                                                                                        document.getElementById('g').style.display = "none"
+                                                                                        document.getElementById('h').style.display = "none"
+                                                                                        document.getElementById('i').style.display = "none"
+                                                                                        document.getElementById('j').style.display = "none"
+                                                                                        document.getElementById('k').style.display = "none"
+                                                                                        document.getElementById('l').style.display = "none"
+                                                                                        document.getElementById('m').style.display = "none"
+                                                                                        document.getElementById('n').style.display = "none"
+                                                                                        document.getElementById('o').style.display = "none"
+                                                                                        document.getElementById('p').style.display = "none"
+                                                                                        document.getElementById('q').style.display = "none"
+                                                                                        document.getElementById('r').style.display = "none"
+                                                                                        document.getElementById('s').style.display = "none"
+                                                                                        document.getElementById('t').style.display = "none"
+                                                                                        document.getElementById('u').style.display = "none"
+
+                                                                                        setTimeout(function() {
+                                                                                            
+                                                                                            Typing();
+
+                                                                                        }, 1000)
+
+                                                                                  }, 1500);
+                                                                            
+                                                                              }, 100);
+                                                                        
+                                                                          }, 100);
+                                                                    
+                                                                      }, 100);
+                                                                
+                                                                  }, 100);
+                                                            
+                                                              }, 100);
+                                                        
+                                                          }, 100);
+                                                    
+                                                      }, 100);
+                                                
+                                                  }, 100);
+                                            
+                                              }, 100);
+                                        
+                                          }, 100);
+                                    
+                                      }, 100);
+                                
+                                  }, 100);
+                            
+                              }, 100);
+                        
+                          }, 100);
+                    
+                      }, 100);
+                
+                  }, 100);
+            
+              }, 100);
+        
+          }, 100);
+    
+      }, 100);
+
+  }, 100);
+
+}
+*/
